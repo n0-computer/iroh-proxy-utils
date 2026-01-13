4e88e502b71c1e680a1a47eb434baea600fd60b3
Frando <frando@unbiskant.org>
2026-01-13 15:31:57 +0100

    cleanup

--- src/gateway.rs
+++ src/gateway.rs
@@ -228,10 +228,7 @@ mod tests {
         use std::convert::Infallible;
 
         use http_body_util::Full;
-        use hyper::body::Bytes;
-        use hyper::server::conn::http1;
-        use hyper::service::service_fn;
-        use hyper::{Request, Response};
+        use hyper::{Request, Response, body::Bytes, server::conn::http1, service::service_fn};
         use hyper_util::rt::TokioIo;
         use tokio::net::TcpListener;
 
@@ -244,19 +241,11 @@ mod tests {
         pub(super) async fn run(
             listener: TcpListener,
         ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-            // We start a loop to continuously accept incoming connections
             loop {
                 let (stream, _) = listener.accept().await?;
-
-                // Use an adapter to access something implementing `tokio::io` traits as if they implement
-                // `hyper::rt` IO traits.
                 let io = TokioIo::new(stream);
-
-                // Spawn a tokio task to serve multiple connections concurrently
                 tokio::task::spawn(async move {
-                    // Finally, we bind the incoming connection to our `hello` service
                     if let Err(err) = http1::Builder::new()
-                        // `service_fn` converts our function in a `Service`
                         .serve_connection(io, service_fn(hello))
                         .await
                     {
